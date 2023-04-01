57927de92d499391f800c116d1aa38377c71c600
Tolani99 <tolaniadisa999@gmail.com>
2023-04-01 20:54:10 +0100

    Formatted diff

--- python_data_representation/project_file_difference/formatted_diff.py
+++ python_data_representation/project_file_difference/formatted_diff.py
@@ -4,4 +4,13 @@ def formatted_diff(idx):
     index up until the first change, then adds a ^.
     """
     return '=' * idx + '^'
+def formatted_diff(idx):
+    """
+    creates the second line of the function singleline_diff_format that places = for every
+    index up until the first change, then adds a ^.
+    """
+    output = ""
+    for _ in range(idx):
+        output = output + "="
+    return output + "^"
 
