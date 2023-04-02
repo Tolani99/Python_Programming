90501bd67ed6e8b5509a94ba1c6f53ee823b0960
Tolani99 <tolaniadisa999@gmail.com>
2023-04-02 08:43:09 +0100

    Dictionaries example

---
+++ python_data_analysis/dictionaries_example.py
@@ -0,0 +1,21 @@
+
+"""
+This implement a simple contact list that maps
+names to phone numbers.
+"""
+#Dictionary Structure
+contacts = {'Scott Rixner': '1-101-555-1234',
+            'Joe Warren': '1-102-555-5678',
+            'Jane Doe': '1-103-555-9012'}
+
+#Value Lookup
+def lookup(contacts, name):
+    """
+    Lookup name in contacts and return phone number.
+    If name is not in contacts, return an empty string.
+    """
+    if name in contacts:
+        return contacts[name]
+    else:
+        return ""
+
