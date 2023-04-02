75d4a815eaad84634d529298dae9b549abf3640f
Tolani99 <tolaniadisa999@gmail.com>
2023-04-02 13:27:11 +0100

    Make a dictionary from a list

--- python_data_analysis/practice_exercise/test.py
+++ python_data_analysis/practice_exercise/test.py
@@ -1,5 +1,2 @@
-my_dict = {"Joe" : 1, "Scott" : 2, "John" : 4}
-sum = 0
-for i in my_dict.values():
-    sum += i
-print(sum)
+key_value = [("Joe", 1), ("Scott", 2), ("John", 4)]
+print(dict(key_value))
